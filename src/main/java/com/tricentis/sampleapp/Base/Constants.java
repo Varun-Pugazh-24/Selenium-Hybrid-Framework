1fc8009ab82c47edaec177d6a8f170dc2a6e794f
varun prasad <varunprasadi10@gmail.com>
2020-12-26 06:02:33 +0530

    Updated driver and properties file path

--- src/main/java/com/tricentis/sampleapp/Base/Constants.java
+++ src/main/java/com/tricentis/sampleapp/Base/Constants.java
@@ -3,7 +3,7 @@ package com.tricentis.sampleapp.Base;
 public class Constants {
 	
 	
-	public static final String SCENARIO_SHEET_PATH = "../KeywordDrivenHubSpot/ExcelFiles/TestCases.xlsx";
-	public static final String CONFIG_PROPERTIES_FILE_PATH = "../KeywordDrivenHubSpot/Configuration/config.properties";
+	public static final String SCENARIO_SHEET_PATH = "./ExcelFiles/TestCases.xlsx";
+	public static final String CONFIG_PROPERTIES_FILE_PATH = "./Configuration/config.properties";
 
 }
